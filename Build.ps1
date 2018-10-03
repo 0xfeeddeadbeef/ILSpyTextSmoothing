cd1a97bea78465ec43e853a836ac625fe57b9724
George Chakhidze <0xfeeddeadbeef@gmail.com>
2018-10-04 00:40:57 +0400

    Fix paths in appveyor scripts

--- Build.ps1
+++ Build.ps1
@@ -17,18 +17,18 @@ $Local:Asset = $Local:LatestRelease.assets |
     Where-Object -FilterScript { ($_.content_type -eq 'application/x-zip-compressed') -and ($_.name -like '*zip') } |
     Select-Object -First 1
 
-$Local:ZipPath = "$env:APPVEYOR_BUILD_FOLDER\ILSpy.zip"
+$Local:ZipPath = "C:\Projects\ILSpyTextSmoothing\ILSpy.zip"
 
 Invoke-WebRequest -UseBasicParsing -Uri $Local:Asset.browser_download_url -Method Get -OutFile $Local:ZipPath
 
 Import-Module Microsoft.PowerShell.Archive
 
-New-Item -Path "$env:APPVEYOR_BUILD_FOLDER\lib\" -ItemType Directory
+New-Item -Path "C:\Projects\ILSpyTextSmoothing\lib\" -ItemType Directory -Verbose | Out-Null
 
-Expand-Archive -Path $Local:ZipPath -DestinationPath "$env:APPVEYOR_BUILD_DIR\lib\" -Force -ErrorAction Stop
+Expand-Archive -Path $Local:ZipPath -DestinationPath "C:\Projects\ILSpyTextSmoothing\lib\" -Force -ErrorAction Stop -Verbose
 
-$Local:CSProj = "$env:APPVEYOR_BUILD_DIR\ILSpyTextSmoothing\ILSpyTextSmoothing\ILSpyTextSmoothing-CI.csproj"
+$Local:CSProj = "C:\Projects\ILSpyTextSmoothing\ILSpyTextSmoothing\ILSpyTextSmoothing-CI.csproj"
 
-((Get-Content -Path $Local:CSProj -Raw -Encoding UTF8) -replace '%PATH_ICSharpCode.AvalonEdit.dll%',"$env:APPVEYOR_BUILD_DIR\lib\ICSharpCode.AvalonEdit.dll") | Set-Content -Path $Local:CSProj -Encoding UTF8
-((Get-Content -Path $Local:CSProj -Raw -Encoding UTF8) -replace '%PATH_ILSpy.exe%',"$env:APPVEYOR_BUILD_DIR\lib\ILSpy.exe") | Set-Content -Path $Local:CSProj -Encoding UTF8
-((Get-Content -Path $Local:CSProj -Raw -Encoding UTF8) -replace '%PATH_Microsoft.VisualStudio.Composition.dll%',"$env:APPVEYOR_BUILD_DIR\lib\Microsoft.VisualStudio.Composition.dll") | Set-Content -Path $Local:CSProj -Encoding UTF8
+((Get-Content -Path $Local:CSProj -Raw -Encoding UTF8) -replace '%PATH_ICSharpCode.AvalonEdit.dll%',"C:\Projects\ILSpyTextSmoothing\lib\ICSharpCode.AvalonEdit.dll") | Set-Content -Path $Local:CSProj -Encoding UTF8
+((Get-Content -Path $Local:CSProj -Raw -Encoding UTF8) -replace '%PATH_ILSpy.exe%',"C:\Projects\ILSpyTextSmoothing\lib\ILSpy.exe") | Set-Content -Path $Local:CSProj -Encoding UTF8
+((Get-Content -Path $Local:CSProj -Raw -Encoding UTF8) -replace '%PATH_Microsoft.VisualStudio.Composition.dll%',"C:\Projects\ILSpyTextSmoothing\lib\Microsoft.VisualStudio.Composition.dll") | Set-Content -Path $Local:CSProj -Encoding UTF8
